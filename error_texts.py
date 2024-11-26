844a8cf00dfd56aca027f6b667ef43ac5e69ea05
harsheenkohli <harsheenkohli2003@gmail.com>
2024-11-27 00:03:00 +0530

    Adding more error texts

--- error_texts.py
+++ error_texts.py
@@ -6,7 +6,14 @@ request_exception_responses = [
     "Oops, I couldn't connect to the servers. Please check back soon!",
     "The network seems a bit slow. I couldn't fetch the data, but I'll try harder next time!",
     "Something went wrong with the connection. Could you retry in a bit?",
-    "I'm having trouble reaching the data source right now. Apologies for the inconvenience!"
+    "I'm having trouble reaching the data source right now. Apologies for the inconvenience!",
+    "It seems there's a temporary network issue on my end. I'll get this resolved as soon as I can!",
+    "Sorry, I couldn't retrieve the information due to a connection problem. Please try again shortly.",
+    "There's a slight hiccup with the network. Could we give it another go later?",
+    "I ran into some technical difficulties while trying to connect. Thanks for your patience!",
+    "It looks like my servers are having a tough time. I appreciate your understanding!",
+    "I couldn't establish a connection this time. Let's try again in a moment.",
+    "It seems like I'm offline at the moment. Let's reconnect soon!"
 ]
 
 key_error_responses = [
@@ -17,5 +24,13 @@ key_error_responses = [
     "Some data I need to answer your query is missing. Please try again.",
     "I couldn't find the details you're asking for. My apologies!",
     "It seems the requested information isn't available at the moment. Sorry!",
-    "The information you're looking for seems to be missing from my sources. My apologies!"
+    "The information you're looking for seems to be missing from my sources. My apologies!",
+    "I looked everywhere I could, but the data doesn't seem to be available. Could you check again?",
+    "Unfortunately, I couldn't track down the specific information you need. My apologies for that!",
+    "Hmm, the requested data appears to be missing or unavailable. Can you try phrasing it differently?",
+    "I tried to fetch the details you need, but they don't seem to exist in my current data.",
+    "I'm struggling to find the exact information you're asking for. Would you mind clarifying or rephrasing?",
+    "Apologies, but I couldn't match your query with any available information. Let's try again!",
+    "I couldn't retrieve the exact data right now. Could you confirm or modify your request?"
 ]
+
